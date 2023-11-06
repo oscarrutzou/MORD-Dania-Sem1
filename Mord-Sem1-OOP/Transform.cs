4e9a399be5ad6928bdaeb1b19eff47a5bedb5e0c
oscarrutzou <91065000+oscarrutzou@users.noreply.github.com>
2023-11-06 12:46:02 +0100

    Transform/Collision

--- Mord-Sem1-OOP/Transform.cs
+++ Mord-Sem1-OOP/Transform.cs
@@ -9,12 +9,8 @@ namespace MordSem1OOP
 {
     public class Transform
     {
-        private Vector2 position;
-        private float rotation;
-        private float scale;
-
-        public Vector2 Position { get => position; set => position = value; }
-        public float Rotation { get => rotation; set => rotation = value; }
-        public float Scale { get => scale; set => scale = value; }
+        public Vector2 Position { get; set; }
+        public float Rotation { get; set; }
+        public float Scale { get; set; }
     }
 }
