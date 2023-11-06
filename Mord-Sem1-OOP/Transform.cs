1516427b1f4542bb36a892aefa0e63118da9f86c
oscarrutzou <91065000+oscarrutzou@users.noreply.github.com>
2023-11-06 12:30:41 +0100

    Transform

---
+++ Mord-Sem1-OOP/Transform.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MordSem1OOP
+{
+    public class Transform
+    {
+        private Vector2 position;
+        private float rotation;
+        private float scale;
+
+        public Vector2 Position { get => position; set => position = value; }
+        public float Rotation { get => rotation; set => rotation = value; }
+        public float Scale { get => scale; set => scale = value; }
+    }
+}
