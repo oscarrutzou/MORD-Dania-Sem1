d4d6da682895e70acc80212493976b9f0d8a8036
Michael <micluksen@icloud.com>
2023-11-06 19:58:08 +0100

    add grid & tile

---
+++ Mord-Sem1-OOP/Scripts/Tile.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace MordSem1OOP.Scripts
+{
+    internal class Tile
+    {
+        private Vector2Int _gridPosition;
+        private Vector2 _position;
+
+        public Vector2Int GridPosition { get { return _gridPosition; } }
+        public Vector2 Position { get { return _position; } }
+
+        public Tile(int x, int y, Vector2 position)
+        {
+            _gridPosition = new Vector2Int(x, y);
+            _position = position;
+        }
+
+        public Tile(Vector2Int gridPosition, Vector2 position)
+        {
+            _gridPosition = gridPosition;
+            _position = position;
+        }
+    }
+}
