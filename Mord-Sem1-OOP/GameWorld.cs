1bbfeef830b4e7d6c570297f17eb6b4afe7b3e85
RasmusBayThomsen <mikemocals@gmail.com>
2023-11-07 11:58:25 +0100

    Scene and GameWorld

--- Mord-Sem1-OOP/GameWorld.cs
+++ Mord-Sem1-OOP/GameWorld.cs
@@ -1,42 +1,47 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Mx2L.MonoDebugUI;
+using System.Collections.Generic;
 
 namespace MordSem1OOP
 {
     public class GameWorld : Game
     {
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
+        protected static GraphicsDeviceManager _graphics;
+        protected static SpriteBatch _spriteBatch;
+        private static Scene[] scenes = new Scene[1];
+        private int activeScene; //Used to call the methods in the current scene
+
 
         public GameWorld()
         {
+            scenes[0] = new Scene();
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
 
+        #region Standard Methods
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
             base.Initialize();
+            activeScene = 0;
         }
-
+        
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
+            scenes[activeScene].LoadContent();
         }
 
         protected override void Update(GameTime gameTime)
         {
             InputManager.HandleInput(this);
 
-            // TODO: Add your update logic here
 
+            scenes[activeScene].Update(gameTime);
             base.Update(gameTime);
         }
 
@@ -44,9 +49,52 @@ namespace MordSem1OOP
         {
             GraphicsDevice.Clear(Color.Beige);
 
-            // TODO: Add your drawing code here
 
+            scenes[activeScene].Draw(gameTime);
             base.Draw(gameTime);
         }
+
+        #endregion
+
+        #region Spawning & Despawning Methods
+        /// <summary>
+        /// Adds a GameObject to the list of GameObjects to be added in the next update.
+        /// </summary>
+        /// <param name="gameObject">The GameObject to be added</param>
+        public void Instantiate(GameObject gameObject)
+        {
+            scenes[activeScene].objectsToCreate.Add(gameObject);
+            gameObject.LoadContent(Content);
+        }
+
+        /// <summary>
+        /// Adds a collection of GameObjects to the list of GameObjects to be added in the next update.
+        /// </summary>
+        /// <param name="gameObject">The GameObject collection to be added</param>
+        public void Instantiate(GameObject[] gameObjects)
+        {
+            scenes[activeScene].objectsToCreate.AddRange(gameObjects);
+            foreach (GameObject gameObject in gameObjects) gameObject.LoadContent(Content);
+        }
+
+        /// <summary>
+        /// Adds a GameObject to the list of GameObjects to be removed in the next update.
+        /// </summary>
+        /// <param name="gameObject">The GameObject to be removed</param>
+        public void Destroy(GameObject gameObject)
+        {
+            scenes[activeScene].objectsToDestroy.Add(gameObject);
+        }
+
+        /// <summary>
+        /// Adds a collection of GameObjects to the list of GameObjects to be removed in the next update.
+        /// </summary>
+        /// <param name="gameObject">The GameObject to be removed</param>
+        public void Destroy(GameObject[] gameObjects)
+        {
+            scenes[activeScene].objectsToDestroy.AddRange(gameObjects);
+        }
+        #endregion
+
     }
 }
---
+++ Mord-Sem1-OOP/Scene.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Content;
+
+namespace MordSem1OOP
+{
+    public class Scene : GameWorld
+    {
+        public List<GameObject> gameObjects = new List<GameObject>();
+        public List<GameObject> objectsToCreate = new List<GameObject>();
+        public List<GameObject> objectsToDestroy = new List<GameObject>();
+
+
+        #region Methods
+        protected override void LoadContent()
+        {
+            //Call LoadContent on every GameObject in the active scene.
+            foreach (GameObject gameObject in gameObjects)
+                gameObject.LoadContent(Content);
+        }
+        
+        protected override void Update(GameTime gameTime)
+        {
+            //All GameObjects to be added, are added to the active scene.
+            foreach (GameObject gameObject in objectsToCreate)
+                gameObjects.Add(gameObject);
+            objectsToCreate.Clear();
+
+            //All GameObjects to be destroyed, are removed from the active scene.
+            foreach (GameObject gameObject in objectsToDestroy)
+                gameObjects.Add(gameObject);
+            objectsToDestroy.Clear();
+
+            //Call update on every GameObject in the active scene.
+            foreach (GameObject gameObject in gameObjects)
+                gameObject.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            //Call draw on every GameObject in the active scene.
+            foreach (GameObject gameObject in gameObjects)
+                gameObject.Draw(_spriteBatch);
+        }
+        #endregion
+
+
+    }
+}
