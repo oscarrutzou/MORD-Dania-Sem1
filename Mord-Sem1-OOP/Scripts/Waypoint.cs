50601fc3fe6c54a9d83ad81889311e1796b801e7
Michael <micluksen@icloud.com>
2023-11-08 19:54:51 +0100

    add waypoint system

---
+++ Mord-Sem1-OOP/Scripts/Waypoint.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace MordSem1OOP.Scripts
+{
+    public class Waypoint
+    {
+        private Waypoint _nextWaypoint;
+        private Vector2 _position;
+        private Vector2Int _gridPosition;
+
+        public Vector2 Position { get => _position; }
+        public Vector2Int Vector2Int { get => _gridPosition; }
+
+        public Waypoint(Vector2 position, Vector2Int gridPosition)
+        {
+            _position = position;
+            _gridPosition = gridPosition;
+        }
+
+        public void SetNextWaypoint(Waypoint waypoint)
+        {
+            _nextWaypoint = waypoint;
+        }
+
+        public bool GetNextWaypoint(out Waypoint waypoint)
+        {
+            if (_nextWaypoint is not null)
+                waypoint = _nextWaypoint;
+            else
+                waypoint = this;
+            return true;
+        }
+    }
+}
---
+++ Mord-Sem1-OOP/Scripts/WaypointManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MordSem1OOP.Scripts
+{
+    internal class WaypointManager
+    {
+        Waypoint[] _waypoints;
+
+        public WaypointManager(params Waypoint[] waypoints)
+        {
+            this._waypoints = waypoints;
+        }
+
+        public void ConnectWaypoints()
+        {
+            for (int i = 0; i < _waypoints.Length - 1; i++)
+            {
+                _waypoints[i].SetNextWaypoint(_waypoints[i + 1]);
+            }
+        }
+    }
+}
