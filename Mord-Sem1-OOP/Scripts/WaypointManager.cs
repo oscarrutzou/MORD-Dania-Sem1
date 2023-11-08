ccb8dcb11c55c6c61a12bd212855e3ba19872a0e
Michael <micluksen@icloud.com>
2023-11-08 23:47:21 +0100

    update WaypointManager

--- Mord-Sem1-OOP/Scripts/WaypointManager.cs
+++ Mord-Sem1-OOP/Scripts/WaypointManager.cs
@@ -8,19 +8,26 @@ namespace MordSem1OOP.Scripts
 {
     internal class WaypointManager
     {
-        Waypoint[] _waypoints;
+        private Waypoint[] _waypoints;
 
         public WaypointManager(params Waypoint[] waypoints)
         {
-            this._waypoints = waypoints;
+            _waypoints = waypoints;
         }
 
-        public void ConnectWaypoints()
+        public WaypointManager ConnectWaypoints()
         {
             for (int i = 0; i < _waypoints.Length - 1; i++)
             {
                 _waypoints[i].SetNextWaypoint(_waypoints[i + 1]);
             }
+
+            return this;
+        }
+
+        public Waypoint GetWaypoint(int index)
+        {
+            return _waypoints[index];
         }
     }
 }
