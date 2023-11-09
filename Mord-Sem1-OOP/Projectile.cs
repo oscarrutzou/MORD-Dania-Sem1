cfdac3db6ba7fc9d45747eb26c1566af2ecdedf0
RasmusBayThomsen <mikemocals@gmail.com>
2023-11-09 14:05:03 +0100

    lmao

--- Mord-Sem1-OOP/Projectile.cs
+++ Mord-Sem1-OOP/Projectile.cs
@@ -47,7 +47,7 @@ namespace MordSem1OOP
             direction.Normalize();
 
             // Calculate rotation towards target
-            Rotation = (float)Math.Atan2(direction.Y, direction.X) + MathHelper.PiOver2;
+            RotateTowards(Target.Position);
 
             if (Target != null)
             {
