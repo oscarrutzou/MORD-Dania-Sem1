5ebf4ab7b577287b9ccb0b834491304b961bfdee
Monsieur David <daviddyne@mail.com>
2023-11-10 11:29:53 +0100

    Animated Counter (virker ikke)

---
+++ Mord-Sem1-OOP/AnimatedCounter.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace MordSem1OOP
+{
+    internal class AnimatedCounter : GameObject
+    {
+        public static Texture2D numberPillar { get; private set; } // for storing OOP-Counter-MedTal2.jpg (is in Content-folder)
+        private Vector2[] positions; // for 6 positions for numberPillar
+        private int startValue = 0;
+        private int targetValue; // Target value to animate towards
+        private float transitionSpeed; // Speed between numbers
+
+        /// <summary>
+        /// (Constructor) Defines positions for numberPillar * 6
+        /// </summary>
+        /// <param name="numberPillar"> store OOP-Counter-MedTal2.jpg </param>
+        /// <param name="startingPosition"> example: new Vector2(100, 100) </param>
+        public AnimatedCounter(Texture2D numberPillar, Vector2 startingPosition)
+        {
+            // Define positions for 6 numberPillar's
+            positions = new Vector2[6];
+            for (int i = 0; i < 6; i++)
+            {
+                // Parameters for Vector 2 = x position, y position ("0" means it doesn't change)
+                positions[i] = startingPosition + new Vector2(i * numberPillar.Width, 0);
+            }
+        }
+
+        /// <summary>
+        /// Constructor uden parametre
+        /// </summary>
+        public AnimatedCounter()
+        {
+        }
+
+        public override void LoadContent(ContentManager content)
+        {
+            numberPillar = content.Load<Texture2D>("OOP-Counter-MedTal2");
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            // Draw each numberPillar
+            foreach (var position in positions)
+            {
+                spriteBatch.Draw(numberPillar, position, Color.White);
+            }
+        }
+
+        /// 
+        ///
+    }
+}
--- Mord-Sem1-OOP/GameWorld.cs
+++ Mord-Sem1-OOP/GameWorld.cs
@@ -1,39 +1,55 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using Mx2L.MonoDebugUI;
+using System.Collections.Generic;
 
 namespace MordSem1OOP
 {
     public class GameWorld : Game
     {
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+        private AnimatedCounter counter;
+        public static float DeltaTime {  get; private set; }
 
         public GameWorld()
         {
-            _graphics = new GraphicsDeviceManager(this);
+            graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+            counter = new AnimatedCounter();
         }
 
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            counter = new AnimatedCounter(AnimatedCounter.numberPillar, new Vector2(100, 100));
+
 
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            counter.LoadContent(Content);
+
 
             // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
         {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape)) 
+                Exit();
+
+            DeltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            
             InputManager.HandleInput(this);
+
+
+            counter.Update(gameTime);
 
             // TODO: Add your update logic here
 
@@ -43,6 +59,12 @@ namespace MordSem1OOP
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.Beige);
+
+            spriteBatch.Begin();
+
+            counter.Draw(spriteBatch);
+
+            spriteBatch.End();
 
             // TODO: Add your drawing code here
 
