cf7b7622319a7b3119413df2f4ff9599b5fe28e5
Monsieur David <daviddyne@mail.com>
2023-11-12 18:51:16 +0100

    .

--- Mord-Sem1-OOP/AnimatedCounter.cs
+++ Mord-Sem1-OOP/AnimatedCounter.cs
@@ -7,11 +7,13 @@ namespace MordSem1OOP
 {
     internal class AnimatedCounter : GameObject
     {
-        public static Texture2D numberPillarSprite; // for storing numberPillar.jpg (is in Content-folder)
-        private Vector2[] positions; // for 6 positions for numberPillarSprite
+        public static Texture2D numberPillarSprite; // For storing numberPillarSprite.jpg (is in Content-folder)
+        private Rectangle[] rectanglePositions; // For 6 positions for numberPillarSprite
+        private int[] yPositions; // For refering each box y-positions as their number inside
+        private Rectangle sourceRectangle; // Limits the view to one numberBox
         private int startValue = 0;
         private int targetValue; // Target value to animate towards
-        private float speed = 650; // Animation speed
+        private float speed = 250; // Animation speed
         private int rectangleHeight = 40;
 
 
@@ -46,14 +48,14 @@ namespace MordSem1OOP
         public override void Update(GameTime gameTime)
         {
             // Move the first number pillar upward based on the elapsed time and speed
-            yPositions[0] += speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            yPositions[0] += (int)(speed * gameTime.ElapsedGameTime.TotalSeconds);
             sourceRectangle.Y = (int)yPositions[0];
             // Check if the first number pillar has reached the bottom
             if (yPositions[0] > numberPillarSprite.Height)
             {
                 // If yes, loop it to the bottom of the screen
                 yPositions[0] = GraphicsDeviceManager.DefaultBackBufferHeight;
-                yPositions[0] = 0;
+                yPositions[0] = 40;
             }
 
         }
