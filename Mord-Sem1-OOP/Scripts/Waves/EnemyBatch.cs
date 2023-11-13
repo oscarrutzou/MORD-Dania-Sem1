d814b74ecdd5eaaa5e2dcea9b01a6fc8acc510a0
RasmusBayThomsen <mikemocals@gmail.com>
2023-11-13 14:49:59 +0100

    I'm going crazy

---
+++ Mord-Sem1-OOP/Scripts/Waves/EnemyBatch.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MordSem1OOP.Scripts.Waves
+{
+    public class EnemyBatch
+    {
+
+        SceneData data; //This is used to load the enemies into the list of GameObjects, as well as access the elapsed time between each update.
+        private EnemyType type;
+        private int count;
+        private float interval;
+
+        private bool canSend = false; //This is enabled by Send(), allowing the update to perform its logic
+        private float timer; //This is used in LocalUpdate() to spawn enemies at the appropriate interval.
+
+
+        /// <summary>
+        /// Creates a batch/group of enemies to be spawned during a wave.
+        /// </summary>
+        /// <param name="type">The type of enemy to be spawned from this batch</param>
+        /// <param name="count">How many of these enemies to spawn</param>
+        /// <param name="interval">The time between each spawn</param>
+        public EnemyBatch(EnemyType type, int count, float interval)
+        {
+            data = Global.activeScene.sceneData;
+
+            this.type = type;
+            this.count = count;
+            this.interval = interval;
+        }
+
+        /// <summary>
+        /// Runs until the count of enemies has been exhausted.
+        /// This method is run by a separate thread, so it doesn't halt the program while waiting.
+        /// </summary>
+        public void Update(GameTime gameTime)
+        {
+            if (!canSend || isDone()) return;
+
+
+
+            timer += (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (timer > interval)
+            {
+                //Spawning the enemy
+                Enemy enemy = new Enemy(type, new Vector2(100, 150));
+                data.gameObjectsToAdd.Add(enemy);
+                
+                count--;
+                timer = 0;
+            }
+        }
+
+
+
+
+        /// <summary>
+        /// Makes this batch spawn its enemies, with the set type, count and interval.
+        /// </summary>
+        public void Send()
+        {
+            canSend = true;
+        }
+
+        /// <summary>
+        /// Checks if the EnemyBatch has spawned all its enemies.
+        /// </summary>
+        public bool isDone()
+        {
+            if (count <= 0) return true;
+            return false;
+        }
+
+
+    }
+}
---
+++ Mord-Sem1-OOP/Scripts/Waves/Wave.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace MordSem1OOP.Scripts.Waves
+{
+    public class Wave
+    {
+        private List<EnemyBatch> batches;
+        private List<float> phaseDurations;
+        private float timer;
+        private int currentBatch;
+
+
+
+        private List<Enemy> enemiesInWave; // List to store all enemies in the wave.
+        private bool isWaveComplete; // Flag to indicate if the wave is complete.
+
+        public List<Enemy> EnemiesInWave => enemiesInWave;
+        public bool IsWaveComplete => isWaveComplete;
+
+
+        public Wave()
+        {
+            batches = new List<EnemyBatch>();
+
+            phaseDurations = new List<float>();
+            enemiesInWave = new List<Enemy>();
+            isWaveComplete = false;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            //Stop updating the batches if the wave is done spawning
+            if (IsWaveComplete) return; 
+
+            batches[currentBatch].Update(gameTime);
+
+
+
+            timer += (float)gameTime.ElapsedGameTime.TotalSeconds;
+            //Check if the phase has been active longer than the time until the next phase
+            if (timer > phaseDurations[currentBatch])
+            {
+                timer = 0;
+                currentBatch++;
+
+                //If there are any more batches to be sent, send it. Otherwise tell the WaveManager that the wave is done.
+                if (currentBatch > batches.Count - 1) isWaveComplete = true;
+                else batches[currentBatch].Send();
+            }
+        }
+
+        public void Begin()
+        {
+            timer = 0;
+            currentBatch = 0;
+            batches[0].Send();
+        }
+
+        //This isn't used anywhere
+        public void Spawn(EnemyType enemyType, int rate, int count)
+        {
+            // Create and add enemies of the specified type to the wave.
+            for (int i = 0; i < count; i++)
+            {
+                SceneData tempSceneData = Global.activeScene.sceneData;
+
+                Enemy enemy1 = new Enemy(EnemyType.Strong, new Vector2(100, 50));
+                tempSceneData.gameObjects.Add(enemy1);
+                tempSceneData.enemies.Add(enemy1);
+            }
+        }
+
+
+        /// <summary>
+        /// Used to add a phase to the wave
+        /// </summary>
+        /// <param name="enemyBatch"></param>
+        /// <param name="timeUntilNextPhase"></param>
+        public void AddPhase(EnemyBatch enemyBatch, float timeUntilNextPhase)
+        {
+            batches.Add(enemyBatch);
+            phaseDurations.Add(timeUntilNextPhase);
+        }
+
+
+    }
+}
