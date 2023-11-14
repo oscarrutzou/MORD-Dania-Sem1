0bf5dda766ce6d566112e9e7e8278b3332ee329d
Monsieur David <daviddyne@mail.com>
2023-11-14 21:52:38 +0100

    Clean up with regions

--- Mord-Sem1-OOP/AnimatedCounter.cs
+++ Mord-Sem1-OOP/AnimatedCounter.cs
@@ -8,36 +8,36 @@ namespace MordSem1OOP
 {
     internal class AnimatedCounter : GameObject
     {
+        #region fields
         public static Texture2D numberPillarSprite; // For storing numberPillarSprite.jpg (is in Content-folder)
-        private Rectangle[] rectanglePositions; // For 6 positions for numberPillarSprite
-        private int[] yPositions; // For refering each box y-positions as their number inside
+        private Rectangle[] rectangleNumberPillarPositions; // For 6 positions for numberPillarSprite
+        private int[] yPositions; // For refering each box y-positions seperately - and animating numberPillarSprite
         private Rectangle sourceRectangle; // Limits the view to one numberBox
         private int[] currentValue;
         private int[] targetValue; // Target value to animate towards
-        private float speed = 550; // Animation speed
+        private float speed = 600; // Animation speed
         private int rectangleHeight = 40;
 
-        private float elapsedTime = 0f;
         private int[] loopCount = new int[6];
-        private float delayTime;
         private static Random random = new Random();
 
-        private static int resultNumber = 123456;
+        private static int resultNumber = 003456;
         private static string numberString = resultNumber.ToString();
 
         // Determine how many leading zeros are needed
         private int leadingZeros = Math.Max(0, 6 - numberString.Length);
 
-        private int setLoopAmount = random.Next(3, 8); // For animation
+        private int setLoopAmount = random.Next(3, 6); // For animation
+        #endregion
 
         /// <summary>
-        /// (Constructor) Defines rectanglePositions for numberPillarSprite * 6
+        /// (Constructor) Defines rectangleNumberPillarPositions for numberPillarSprite * 6
         /// </summary>
         /// <param name="startingPosition"> example: new Vector2(100, 100) </param>
         public AnimatedCounter(Vector2 startingPosition)
         {
-            // Define rectanglePositions for 6 numberPillarSprite's
-            rectanglePositions = new Rectangle[6];
+            // Define rectangleNumberPillarPositions for 6 numberPillarSprite's
+            rectangleNumberPillarPositions = new Rectangle[6];
             sourceRectangle = new Rectangle(1, 0, numberPillarSprite.Width, rectangleHeight);
             yPositions = new int[6];
             currentValue = new int[6];
@@ -47,50 +47,15 @@ namespace MordSem1OOP
 
             for (int i = 0; i < 6; i++)
             {
-                // Parameters for Vector 2 = x position, y position ("0" means it doesn't change)
-                rectanglePositions[i] = new Rectangle(numberPillarSprite.Width * i, (int)startingPosition.Y, numberPillarSprite.Width, numberPillarSprite.Height / 9);
-
+                rectangleNumberPillarPositions[i] = new Rectangle(numberPillarSprite.Width * i, (int)startingPosition.Y, numberPillarSprite.Width, numberPillarSprite.Height / 9);
             }
         }
-
-        private int GetIndexPosition(int index)
-        {
-            return index * rectangleHeight;
-        }
-
-        public override void LoadContent(ContentManager content)
-        {
-            numberPillarSprite = content.Load<Texture2D>("numberPillarSprite");
-        }
-
-        public override void Update(GameTime gameTime)
-        {
-            HandleInput(gameTime);
-            Animation(gameTime);
-            //delayTime = random.Next(3, 8);
-            //bool loop = true;
-            //elapsedTime += (float)gameTime.ElapsedGameTime.TotalSeconds;
-            //if (elapsedTime >= delayTime)
-            //{
-            //    loop = false;
-            //    elapsedTime = 0f;
-            //    delayTime = (float)random.NextDouble() * (8-3) + 3;
-            //}
-        }
-        public void Draw(SpriteBatch spriteBatch)
-        {
-
-            for (int i = 0; i < rectanglePositions.Length; i++)
-            {
-                sourceRectangle.Y = yPositions[i];
-                spriteBatch.Draw(numberPillarSprite, rectanglePositions[i], sourceRectangle, Color.White);
-            }
-        }
-
         public void Animation(GameTime gameTime)
         {
-            for (int i = 0; i < rectanglePositions.Length; i++)
+            // loops from 0 to the number of numberPillars (their rectangle) 
+            for (int i = 0; i < rectangleNumberPillarPositions.Length; i++)
             {
+                // skips rest of loop if loopCount has reached setLoopAmount
                 if (loopCount[i] >= setLoopAmount)
                 {continue; }
 
@@ -98,8 +63,10 @@ namespace MordSem1OOP
                 int min = targetValue[i] * rectangleHeight;
                 int max = (targetValue[i] + 1) * rectangleHeight;
 
+                // If yPositions is less than min or above max
                 if (!InRange(yPositions[i], min, max))
                 {
+                    // Handle wrap-around if newYPosition exceeds the sprite height
                     if (newYPosition > numberPillarSprite.Height - rectangleHeight)
                     {
                         newYPosition = newYPosition - numberPillarSprite.Height + rectangleHeight;
@@ -107,16 +74,17 @@ namespace MordSem1OOP
 
                     if (InRange(newYPosition, min, max))
                     {
-                        if (loopCount[i] == 3 - 1)
+                        if (loopCount[i] == setLoopAmount - 1)
                         {
+                            // = targetValue[i] * 40 (rectangleHeight)
                             newYPosition = GetIndexPosition(targetValue[i]);
                         }
                         loopCount[i]++;
                     }
                 }
 
+                // Animates each numberPillar - updates for each "speed * gameTime.ElapsedGameTime.TotalSeconds" (frame?)
                 yPositions[i] = newYPosition;
-
                 sourceRectangle.Y = (int)yPositions[i];
                 currentValue[i] = (int)(yPositions[i] / rectangleHeight);
 
@@ -129,7 +97,19 @@ namespace MordSem1OOP
                 }
             }
         }
+        #region other
+        private int GetIndexPosition(int index)
+        {
+            return index * rectangleHeight;
+        }
 
+        /// <summary>
+        /// Returns true if value is between or equals to min and max
+        /// </summary>
+        /// <param name="value">check if this int is between or equal to min and max</param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <returns></returns>
         public bool InRange(int value, int min, int max)
         {
             return min <= value && value <= max;
@@ -150,15 +130,40 @@ namespace MordSem1OOP
 
         public void TranslateResultNumberToArray()
         {
+            // Assigns the 0's to the left of the number (if leadingZeroes is more than 0 (meaning: resultNumber has at least one "0"))
             for (int i = 0; i < leadingZeros; i++)
             {
                 targetValue[i] = 0;
             }
-            // Assign the remaining digits to the array
+            // Assigns the remaining digits to the array
             for (int i = leadingZeros; i < targetValue.Length; i++)
             {
                 targetValue[i] = int.Parse(numberString[i - leadingZeros].ToString());
             }
         }
+        #endregion
+        #region standard stuff
+
+        public override void LoadContent(ContentManager content)
+        {
+            numberPillarSprite = content.Load<Texture2D>("numberPillarSprite");
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            HandleInput(gameTime);
+            Animation(gameTime);
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+            for (int i = 0; i < rectangleNumberPillarPositions.Length; i++)
+            {
+                sourceRectangle.Y = yPositions[i];
+                spriteBatch.Draw(numberPillarSprite, rectangleNumberPillarPositions[i], sourceRectangle, Color.White);
+            }
+        }
+
+        #endregion
     }
 }
