12a6e40b25f1dc29dcf008ef75aa0767b5472d51
Monsieur David <daviddyne@mail.com>
2023-11-14 13:39:24 +0100

    TranslateResultNumberToArray method in constructor + fields: resultNumber, numberString, leadingZeros

--- Mord-Sem1-OOP/AnimatedCounter.cs
+++ Mord-Sem1-OOP/AnimatedCounter.cs
@@ -2,7 +2,6 @@
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-
 using System;
 
 namespace MordSem1OOP
@@ -15,18 +14,21 @@ namespace MordSem1OOP
         private Rectangle sourceRectangle; // Limits the view to one numberBox
         private int[] currentValue;
         private int[] targetValue; // Target value to animate towards
-        private float speed = 200; // Animation speed
+        private float speed = 550; // Animation speed
         private int rectangleHeight = 40;
 
         private float elapsedTime = 0f;
         private int[] loopCount = new int[6];
         private float delayTime;
-        private Random random = new Random();
+        private static Random random = new Random();
 
-        private static int resultNumber = 456;
-        private string numberString = resultNumber.ToString();
+        private static int resultNumber = 123456;
+        private static string numberString = resultNumber.ToString();
 
+        // Determine how many leading zeros are needed
+        private int leadingZeros = Math.Max(0, 6 - numberString.Length);
 
+        private int setLoopAmount = random.Next(3, 8); // For animation
 
         /// <summary>
         /// (Constructor) Defines rectanglePositions for numberPillarSprite * 6
@@ -39,15 +41,15 @@ namespace MordSem1OOP
             sourceRectangle = new Rectangle(1, 0, numberPillarSprite.Width, rectangleHeight);
             yPositions = new int[6];
             currentValue = new int[6];
-            targetValue = new int[] { 9, 8, 4, 5, 6, 7 };
+            targetValue = new int[6];
+
+            TranslateResultNumberToArray();
 
             for (int i = 0; i < 6; i++)
             {
                 // Parameters for Vector 2 = x position, y position ("0" means it doesn't change)
                 rectanglePositions[i] = new Rectangle(numberPillarSprite.Width * i, (int)startingPosition.Y, numberPillarSprite.Width, numberPillarSprite.Height / 9);
 
-                // Limits the view of numberPillarSprite to one numberBox
-                
             }
         }
 
@@ -74,8 +76,6 @@ namespace MordSem1OOP
             //    elapsedTime = 0f;
             //    delayTime = (float)random.NextDouble() * (8-3) + 3;
             //}
-
-
         }
         public void Draw(SpriteBatch spriteBatch)
         {
@@ -91,32 +91,10 @@ namespace MordSem1OOP
         {
             for (int i = 0; i < rectanglePositions.Length; i++)
             {
-                // Move the first number pillar upward based on the elapsed time and speed
-
-                //if (currentValue[i] == targetValue[i])
-                //    continue;
-
-                //if (currentValue[i] != targetValue[i])
-                //{
-                //    //loopCount[i]++;
-                //    continue;
-                //}
-
-                if (loopCount[i] >= 3)
-                {
-                    //loopCount[i] = 0;
-                    continue; }
+                if (loopCount[i] >= setLoopAmount)
+                {continue; }
 
                 int newYPosition = yPositions[i] + (int)(speed * gameTime.ElapsedGameTime.TotalSeconds);
-
-                //if (currentValue[i] != targetValue[i] && (int)(newYPosition / rectangleHeight) == targetValue[i])
-
-                // Iterate through each character in the string and assign it to the corresponding position in the array
-                for (int i2 = 0; i2 < Math.Min(numberString.Length, targetValue.Length); i2++)
-                {
-                    // Convert the character to an integer and assign it to the array
-                    targetValue[i2] = int.Parse(numberString[i2].ToString());
-                }
                 int min = targetValue[i] * rectangleHeight;
                 int max = (targetValue[i] + 1) * rectangleHeight;
 
@@ -137,9 +115,7 @@ namespace MordSem1OOP
                     }
                 }
 
-
                 yPositions[i] = newYPosition;
-                //yPositions[i] += (int)(speed * gameTime.ElapsedGameTime.TotalSeconds);
 
                 sourceRectangle.Y = (int)yPositions[i];
                 currentValue[i] = (int)(yPositions[i] / rectangleHeight);
@@ -153,49 +129,6 @@ namespace MordSem1OOP
                 }
             }
         }
-
-        //int[] debugCurr = new int[6];
-        //int[] debugNew = new int[6];
-        //int[] debugMin = new int[6];
-        //int[] debugMax = new int[6];
-        //public void AddToDebug()
-        //{
-        //    DebugInfo.AddString("debugCurr", DebugMethod1);
-        //    DebugInfo.AddString("debugNew", DebugMethod2);
-        //    DebugInfo.AddString("debugMin", DebugMethod3);
-        //    DebugInfo.AddString("debugMax", DebugMethod4);
-        //    DebugInfo.AddString("debugBoolCur", DebugMethod5);
-        //    DebugInfo.AddString("debugBoolNew", DebugMethod5);
-        //}
-        //public string DebugMethod1()
-        //{
-        //    return debugCurr[0].ToString();
-        //}
-
-        //public string DebugMethod2()
-        //{
-        //    return debugNew[0].ToString();
-        //}
-
-        //public string DebugMethod3()
-        //{
-        //    return debugMin[0].ToString();
-        //}
-
-        //public string DebugMethod4()
-        //{
-        //    return debugMax[0].ToString();
-        //}
-
-        //public string DebugMethod5()
-        //{
-        //    return $"{debugMin[0]} <= {debugCurr[0]} <= {debugMax[0]}" + " " + InRange(debugNew[0], debugMin[0], debugMax[0]);
-        //}
-
-        //public string DebugMethod6()
-        //{
-        //    return $"{debugMin[0]} <= {debugNew[0]} <= {debugMax[0]}" + " " + InRange(debugNew[0], debugMin[0], debugMax[0]);
-        //}
 
         public bool InRange(int value, int min, int max)
         {
@@ -215,7 +148,17 @@ namespace MordSem1OOP
             }
         }
 
-        /// 
-        ///
+        public void TranslateResultNumberToArray()
+        {
+            for (int i = 0; i < leadingZeros; i++)
+            {
+                targetValue[i] = 0;
+            }
+            // Assign the remaining digits to the array
+            for (int i = leadingZeros; i < targetValue.Length; i++)
+            {
+                targetValue[i] = int.Parse(numberString[i - leadingZeros].ToString());
+            }
+        }
     }
 }
